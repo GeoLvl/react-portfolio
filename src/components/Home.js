9db1fd7e6826fedf3281ae6bb43051b053df8d09
Geoffrey Laville <laville.geoffrey@gmail.com>
2022-01-17 17:16:18 +0100

    setup basic start & components

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+const home = () => {
+  return (
+    <div>
+      <div>
+        <h3>Welcome !</h3>
+        <h1>I'm Geoffrey</h1>
+        <p>Front-end developper</p>
+      </div>
+    </div>
+  )
+}
+ 
+export default home;
