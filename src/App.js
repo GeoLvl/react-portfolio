eb97720c9c334932567e0bfbbbf10f804dd95ae8
Geoffrey Laville <laville.geoffrey@gmail.com>
2022-01-18 18:00:30 +0100

    footer component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <footer className="foot-style wrapper">
+      <hr />
+      <div className="foot-container">
+          <p>made with love</p>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
