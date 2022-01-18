cd3c9b8fa84e23d284a92fe9aff39c1c3f713e48
Geoffrey Laville <laville.geoffrey@gmail.com>
2022-01-18 17:43:46 +0100

    homepage content & css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,27 @@
-import React from 'react';
- 
+import React from "react";
+
 const home = () => {
   return (
-    <div>
-      <div>
-        <h3>Welcome !</h3>
-        <h1>I'm Geoffrey</h1>
-        <p>Front-end developper</p>
+    <div className="wrapper">
+      <div className="home-container">
+        <h3 className="big-title">Welcome</h3>
+        <div className="intro">
+          <h2 className="little-title-1">Hey ! I'm Geoffrey</h2>
+          <h1 className="little-title-2">Web developer & Graphic designer</h1>
+          
+          <h5 className="little-title-3">I know how to use : </h5>
+          <p>HTML/CSS, Javascript, React, NodeJS, Express, MongoDB, <br/>
+          Adobe Creative Suite, SEO & SEA.</p>
+          <p>Currently learning Sass & Typescript !</p>
+
+          <h5 className="little-title-3">Me in 12 words : </h5>
+          <p>I don't really like Comic Sans MS, I'm passionate about web & love good design !<br/>
+          I'm also good at maths.</p>
+          <p>Welcome to my world !</p>
+        </div>
       </div>
     </div>
-  )
-}
- 
+  );
+};
+
 export default home;
